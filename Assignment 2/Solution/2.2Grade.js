23c2333bb029574080dce04efb184475e2ff9032
Siddharth Priyadarshi <sidmail4606@gmail.com>
2022-10-14 17:29:52 +0530

    Refactor: if-else to switch

--- Assignment 2/Solution/2.2Grade.js
+++ Assignment 2/Solution/2.2Grade.js
@@ -1,37 +1,42 @@
 // Write a function using switch case to find the grade of a student based
 // on marks obtained
 
-function gradeAllotment( mark ){
-    let grade;
-    if( mark>=91 && mark<=100){
-        grade = 'S';
-    }
-    else if( mark>=81 && mark <= 90){
-        grade = 'A';
-    }
-    else if( mark>=71 && mark <= 80){
-        grade = 'B';
-    }
-    else if( mark>=61 && mark <= 70){
-        grade = 'C';
-    }
-    else if( mark>=51 && mark <= 60){
-        grade = 'D';
-    }
-    else if( mark>=41 && mark <= 50){
-        grade = 'E';
-    }
-    else if( mark>=0 && mark<=40){
-        grade = 'F';
-    }
-    else{
-        return "INVALID MARKS"
-    }
 
-    return grade;
+
+function gradeAllotment( mark ){
+    let grade = "INVALID MARKS";
+    if( mark >= 0 && mark <= 100){
+        mark = Math.floor(mark / 10);
+        switch( mark){
+            case 10:
+            case 9:
+                grade = 'S'; break;
+            case 8:
+                grade = 'A'; break;
+            case 7:
+                grade = 'B'; break;
+            case 6:
+                grade = 'C'; break;
+
+            case 5:
+                grade = 'D'; break;
+            case 4:
+                grade = 'E'; break;
+            case 3:
+            case 2: 
+            case 1: 
+            case 0:
+                grade = 'F'; break;
+            default: 
+                grade = "INVALID MARKS";
+        }
+    }
+    return grade;    
 }
 
 console.log( gradeAllotment(91))
 console.log( gradeAllotment(101))
 console.log( gradeAllotment(-1))
 console.log( gradeAllotment(75))
+
+
